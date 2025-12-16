18f62f24a5f62955dfd5e0807354448a749b438f
Navyainukonda <navyainukonda0509@gmail.com>
2025-12-17 03:49:28 +0530

    Auto-generated commit

---
+++ chunk-RMVSMTUQ.js
@@ -0,0 +1,1 @@
+import{Aa as o,Ua as n,Va as p,fb as m}from"./chunk-6DBKFS3D.js";var l=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-employee"]],decls:2,vars:0,template:function(e,a){e&1&&(n(0,"p"),m(1,"employee works!"),p())},encapsulation:2})};export{l as Employee};
